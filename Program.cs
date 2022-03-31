260681bede79e47874cbdf5f47b429f66b997edb
Yuvaraj <86014715+yuvaraj39@users.noreply.github.com>
2022-04-01 00:49:32 +0530

    databseConnection

--- Program.cs
+++ Program.cs
@@ -2,5 +2,49 @@
 Author: Yuvaraj J
 Description: Electronics shopping site
 Modified by:Yuvaraj J
-Modified at:31/03/2022 8:51 PM
+Modified at:31/03/2022 7:00 PM
 */
+
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System;
+
+namespace MySQLConnectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connStr = "server=localhost;user=root;database=shopping;port=3306;password=1999";
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                SqlParameter param1 = new SqlParameter();
+                    param1.ParameterName = "@employeeID";
+                    param1.SqlDbType = SqlDbType.Int;
+                    param1.Value = int.Parse(args[0].ToString());
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = "select * from shopping.login where Username=@Uvaraja";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ae)
+            {
+                Console.WriteLine(ae.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Connection Closed. Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
